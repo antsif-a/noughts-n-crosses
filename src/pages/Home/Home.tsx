66ed27779808ae9929859d67bcf5b5544fb4ac2f
antsif-a <summet.dev@gmail.com>
2022-07-07 19:03:52 +0300

    refactor: implement pages

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Board from '../../components/Board';
+import Info from '../../components/Info';
+import { CellData } from '../../types/CellData';
+import { Player } from '../../types/Player';
+import './Home.scss';
+
+function initialCells(quantity = 9): CellData[] {
+    const cells: CellData[] = [];
+    for (let i = 0; i < quantity; i++) {
+        cells.push({
+            id: i,
+            state: null,
+        });
+    }
+
+    return cells;
+}
+
+export default function Home() {
+    const [turn, setTurn] = useState(Player.X);
+    const [cells, setCells] = useState(initialCells());
+
+    const onReset = () => {
+        setTurn(Player.X);
+        setCells(initialCells());
+    };
+
+    return (
+        <>
+            <h1 className="title">Tic-tac-toe</h1>
+            <div className="container">
+                <Board
+                  turn={turn}
+                  setTurn={setTurn}
+                  cells={cells}
+                  setCells={setCells}
+                />
+                <Info
+                  turn={turn}
+                  onReset={onReset}
+                />
+            </div>
+        </>
+    );
+}
