a716fdb0719c398b1975bb310fd5a9fd5a570b65
antsif-a <summet.dev@gmail.com>
2022-07-08 03:50:46 +0300

    feat: implement SVG icons loading

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,29 @@ module.exports = {
           },
         }],
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              prettier: false,
+              svgo: false,
+              svgoConfig: {
+                plugins: [{ removeViewBox: false }],
+              },
+              titleProp: true,
+              ref: true,
+            },
+          },
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'static/media/[name].[hash].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
 };
