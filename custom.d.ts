f15625bc4336c1dafbf9aa840ecaa6feefe9e4bc
antsif-a <summet.dev@gmail.com>
2022-07-08 03:49:26 +0300

    refactor: add better types for extensions modules

--- custom.d.ts
+++ custom.d.ts
@@ -1,3 +1,18 @@
-declare module '*.scss';
-declare module '*.sass';
-declare module '*.css';
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare module '*.svg' {
+    import * as React from 'react';
+
+    export const ReactComponent: React.FunctionComponent<
+        React.SVGProps<SVGSVGElement> & { title?: string }
+        >;
+
+    const src: string;
+    export default src;
+}
+
+declare module '*.module.scss' {
+    const classes: { readonly [key: string]: string };
+    export default classes;
+}
